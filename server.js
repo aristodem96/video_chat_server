04f94df15884bb9fa1b048e96f7ebd8723339a57
aristodem <aristodemit@gmail.com>
2025-02-23 22:06:35 +0200

    new_variant_with_rooms

--- server.js
+++ server.js
@@ -6,58 +6,77 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-// CSP
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-  );
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" },
 });
 
-let waitingUser = null;
+// Храним комнаты и пользователей
+let rooms = {};  
+
+// Генерация случайного ID для комнаты
+const generateRoomId = () => Math.random().toString(36).substring(7);
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  if (waitingUser) {
-    socket.emit("peer-connected", { peerId: waitingUser });
-    io.to(waitingUser).emit("peer-connected", { peerId: socket.id });
-    waitingUser = null;
-  } else {
-    waitingUser = socket.id;
+  // Ищем подходящую комнату для подключения
+  let assignedRoom = null;
+  for (const roomId in rooms) {
+    if (rooms[roomId].length < 2) {
+      assignedRoom = roomId;  // Если в комнате есть место, назначаем пользователя
+      break;
+    }
   }
 
+  // Если подходящей комнаты нет, создаём новую
+  if (!assignedRoom) {
+    assignedRoom = generateRoomId();
+    rooms[assignedRoom] = [];
+  }
+
+  // Добавляем пользователя в комнату
+  rooms[assignedRoom].push(socket.id);
+  socket.join(assignedRoom);
+  console.log(`User ${socket.id} assigned to room ${assignedRoom}`);
+
+  // Если в комнате 2 человека, начинаем соединение
+  if (rooms[assignedRoom].length === 2) {
+    io.to(rooms[assignedRoom][0]).emit("peer-connected", { peerId: rooms[assignedRoom][1] });
+    io.to(rooms[assignedRoom][1]).emit("peer-connected", { peerId: rooms[assignedRoom][0] });
+    console.log(`Room ${assignedRoom} is ready for call`);
+  }
+
+  // Обработка предложения на звонок
   socket.on("offer", ({ offer, recipientId }) => {
     io.to(recipientId).emit("offer", { offer, senderId: socket.id });
   });
 
+  // Обработка ответа на предложение
   socket.on("answer", ({ answer, recipientId }) => {
     io.to(recipientId).emit("answer", { answer });
   });
 
+  // Обработка ICE-кандидатов
   socket.on("ice-candidate", ({ candidate, recipientId }) => {
     io.to(recipientId).emit("ice-candidate", { candidate });
   });
 
+  // Удаление пользователя из комнаты при отключении
   socket.on("disconnect", () => {
     console.log(`User disconnected: ${socket.id}`);
-    if (waitingUser === socket.id) waitingUser = null;
+    // Убираем пользователя из комнаты
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
+      if (rooms[roomId].length === 0) {
+        delete rooms[roomId];  // Удаляем пустую комнату
+      }
+    }
   });
 });
 
 const PORT = process.env.PORT || 5001;
-const HOST = process.env.HOST || "0.0.0.0"; // Указываем 0.0.0.0 для работы с публичным IP
+const HOST = process.env.HOST || "0.0.0.0"; 
 
 server.listen(PORT, HOST, () => {
   console.log(`🚀 Server running on http://${HOST}:${PORT}`);
